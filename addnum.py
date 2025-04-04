574bf0f836d6c0b58a08acba806015d3dafb3451
Nandan Kumar <nandankg@gmail.com>
2025-04-04 11:11:27 +0530

    New File added

---
+++ addnum.py
@@ -0,0 +1,4 @@
+num1=int(input("Enter First number"))
+num2=int(input("Enter second number"))
+s=num1+num2
+print("Sum of ",num1 ," and ",num2," is ",s)
--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
 print("Hello World")
+print("Welcome to DevOps Class")
